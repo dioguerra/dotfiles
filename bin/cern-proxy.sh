c4b548c35f06fc112a08fd4fba9d31869fcc06a1
Diogo Guerra <dy090.guerra@gmail.com>
2021-03-03 16:19:15 +0100

    Fix cern proxy script

--- bin/cern-proxy.sh
+++ bin/cern-proxy.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 case $1 in
     connect)
-        sshuttle --ns-hosts 137.138.16.5 -D -vr dtomasgu@lxplus-cloud.cern.ch 137.138.0.0/16 128.141.0.0/16 128.142.0.0/16 188.184.0.0/15 --daemon --pidfile /tmp/cern-proxy
+        sshuttle --dns -D -vr dtomasgu@lxplus-cloud.cern.ch 137.138.0.0/16 128.141.0.0/16 128.142.0.0/16 188.184.0.0/15 --daemon --pidfile /tmp/cern-proxy
         shift
     ;;
     disconnect)
