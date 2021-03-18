c18a6dd4b28294b95b2fe22768352778fd949dcc
Diogo Guerra <dy090.guerra@gmail.com>
2021-03-18 16:58:20 +0100

    Update cern tunnel to use lxplus8

--- bin/cern-proxy.sh
+++ bin/cern-proxy.sh
@@ -2,7 +2,7 @@
 
 case $1 in
     connect)
-        sshuttle --dns -D -vr dtomasgu@lxplus-cloud.cern.ch 137.138.0.0/16 128.141.0.0/16 128.142.0.0/16 188.184.0.0/15 --daemon --pidfile /tmp/cern-proxy
+        sshuttle --dns -D -vr dtomasgu@lxplus8.cern.ch 137.138.0.0/16 128.141.0.0/16 128.142.0.0/16 188.184.0.0/15 --daemon --pidfile /tmp/cern-proxy
         shift
     ;;
     disconnect)
