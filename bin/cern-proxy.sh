0df16cd542b33ec0f839e3bef7ebf54f64e10f38
Diogo Guerra <diogo.filipe.tomas.guerra@cern.ch>
2021-06-14 15:56:02 +0200

    Fix CERN tunnel server and pid file

--- bin/cern-proxy.sh
+++ bin/cern-proxy.sh
@@ -2,11 +2,11 @@
 
 case $1 in
     connect)
-        sshuttle --dns -D -vr dtomasgu@lxplus8.cern.ch 137.138.0.0/16 128.141.0.0/16 128.142.0.0/16 188.184.0.0/15 --daemon --pidfile /tmp/cern-proxy
+        sshuttle --dns -vr dtomasgu@aiadm.cern.ch 137.138.0.0/16 128.141.0.0/16 128.142.0.0/16 188.184.0.0/15 --daemon --pidfile /tmp/cern-proxy.pid
         shift
     ;;
     disconnect)
-        kill `cat /tmp/cern-proxy`
+        kill `cat /tmp/cern-proxy.pid`
         shift
     ;;
     *)
