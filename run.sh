591c5255e8895fbd93045b3d34b627deaeae19de
Diogo Guerra <dy090.guerra@gmail.com>
2023-09-22 19:29:33 +0200

    Download dotfiles duh!

--- run.sh
+++ run.sh
@@ -6,4 +6,10 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# We need to download dotfiles
+sudo apt install -y git
+mkdir $HOME/ws
+git clone https://github.com/dioguerra/dotfiles.git $HOME/ws/dotfiles
+
+# Start main install
 ./INSTALL.sh
